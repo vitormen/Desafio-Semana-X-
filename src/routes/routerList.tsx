cfb1d59b88633f82c4279f9d0e2fe691ca62e411
“Samuel <samuel_alcantara@outlook.com.br>
2023-09-21 13:55:02 -0300

    :sparkles: feat: Page signUp

--- src/routes/routerList.tsx
+++ src/routes/routerList.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import Edit from "../pages/Edit-Profile";
+import SignUp from "../pages/Sign-up"
 
 export const RouteList = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/edit" element={<Edit />} />
+      <Route path="/sign-up" element={<SignUp />} />
     </Routes>
   );
 };
