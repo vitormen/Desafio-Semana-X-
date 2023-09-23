7dc146bea475010e7291a8757139bf5991d8e0de
vitormen <vm1522279@gmail.com>
2023-09-23 13:02:02 -0300

    💄feat:add select

--- src/pages/Edit-Profile/index.tsx
+++ src/pages/Edit-Profile/index.tsx
@@ -1,10 +1,21 @@
 import Navbar from "../../components/navbar";
 import UserImage from "../../assets/User.png";
-
 import * as C from "./styles";
+import Arrow from "../../assets/CaretDown.png"
+import { useState } from "react"
 
 const Profile = () => {
-  
+  const [selectValue, setSelecValue] = useState<String>("")
+    const [options, setOptions] = useState<Boolean>(false)
+    
+
+    const Options = [
+        {_id: "1", name: "Solteiro" },
+        {_id: "2", name: "Casado"},
+        {_id: "3", name: "Divorciado"},
+        {_id: "4", name: "Namorando"},
+        {_id: "5", name: "Preocupado"}
+    ]
   return (
     <>
       <Navbar
@@ -21,9 +32,29 @@ const Profile = () => {
           </C.LeftProfile>
         </C.EditsAndLeftProfile>
         <C.CenterProfile>
-          <C.title>Editar informações
+            <C.title>Editar informações
           </C.title>
-                        <C.Input widthButton={"13.4375rem"} marginTop={"5.75rem"} type="text" id="Name" name="Name" placeholder="Profissão"></C.Input>
+         <C.divSelect>
+                    <C.AreaSelect onClick={() => setOptions(!options)}>
+                        <C.Select >
+                            {selectValue === ""? "Relacionamento" : selectValue}
+                        </C.Select>
+                        <C.Arrowimg src={Arrow} alt="caretdown" />
+                        {options &&
+                            <C.AreaOptions>
+                                {Options.map((option, index, Options) => (
+                                    <div key={index}>
+                                        <C.Option onClick={() => setSelecValue(option.name)}>{option.name}</C.Option>
+                                        {index !== Options.length - 1 && <C.LineOption></C.LineOption>}
+                                    </div>
+                                    ))}
+                            </C.AreaOptions>
+                        }
+        
+                    </C.AreaSelect>
+                   </C.divSelect>
+                      <C.divInput>
+                         <C.Input widthButton={"13.4375rem"} marginTop={"5.75rem"} type="text" id="Name" name="Name" placeholder="Profissão"></C.Input>
 
 
                         <C.Input widthButton={"13.4375rem"} marginTop={"1.19rem"} type="text" id="Name" name="Name" placeholder="Nome"></C.Input>
@@ -37,7 +68,9 @@ const Profile = () => {
                         <C.Input widthButton={"13.8125rem"} marginTop={"1.5rem"} type="password" id="password" name="password" placeholder="Senha"></C.Input>
 
                         <C.Input widthButton={"13.8125rem"} marginTop={"1.5rem"} type="password" id="password" name="password" placeholder="Repetir senha"></C.Input>
-                    <C.Button>Salvar</C.Button>
+                        </C.divInput>
+                       
+                    <C.Button1>Salvar</C.Button1>
 
                   
           
--- src/pages/Edit-Profile/styles.ts
+++ src/pages/Edit-Profile/styles.ts
@@ -89,13 +89,92 @@ type InputProps = {
   marginTop: string;
   imgInput?: string;
 }
+export const divSelect = styled.div`
+  display: flex;
+  flex-direction: column; 
+  margin-right: 6.63rem;
+`;
+export const AreaSelect = styled.div`
+    position: relative;
+    width: 11.3125rem;
+    height: 2.1875rem;
+    margin-top: 5.75rem;
+    margin-left: 22.94rem;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    background: #EFF3F8;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+`
+export const Select = styled.span`
+    color: #868686;
+    font-size: 1rem;
+    font-family: Roboto Flex;
+`
+export const Arrowimg = styled.img`
+    width: 1.3125rem;
+    height: 1.31131rem;
+`
 
+export const Button = styled.button`
+    width: 21rem;
+    height: 3.1875rem;
+    border-radius: 0.5rem;
+    background: #ED6D25;
+    padding: 0.94rem;
+    align-text: center;
+    color: #FFF;
+    font-size: 1.125rem;
+    border: none;
+    cursor: pointer;
+    margin-top: 2.19rem;
+    font-family: Roboto Flex;
+`
+export const AreaOptions = styled.div`
+    margin-top: 0.13rem;
+    padding: 1rem;
+    width: 11.3125rem;
+    border-radius: 0rem 0rem 0.375rem 0.375rem;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    position: absolute;
+    z-index: 1;
+    top: 100%;
+    right: 0;
+    font-size: 0.875rem;
+
+    
+`
+export const Option = styled.p`
+    color: #8D8D8D;
+    font-family: Roboto Flex;
+    font-size: 0.875rem;
+    margin: 0;
+    padding: 0.12rem 0 0.5rem 0;
+    &:hover{
+        color: #000;
+        font-weight: 400;
+        }
+`
+export const LineOption = styled.div`
+    width: inherit;
+    height: 0.0625rem;
+    background: #ED6D25;
+`;
+
+export const divInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.19rem;
+  margin-left: 2.88rem;
+`;
 export const Input = styled.input<InputProps>` 
     width: ${props => props.widthButton};
     margin-top: ${props => props.marginTop};
     display: flex;
     flex-direction: column;
-    margin-left: 2.88rem;
     height:3.1875rem;
     font-size: 1rem;
     padding: 1rem;
@@ -111,7 +190,8 @@ export const Input = styled.input<InputProps>`
 `;
 
 
-export const Button = styled.button`
+
+export const Button1 = styled.button`
     width: 21rem;
     height: 3.1875rem;
     border-radius: 0.5rem;
