7a65866569df8b6c6a4f5e7e1e6289b208a9e5be
Brak3 <gabrielvieira040@outlook.com>
2023-09-22 21:01:11 -0300

    :sparkles: feat: Html of the page completed

--- src/pages/NewPassword/index.tsx
+++ src/pages/NewPassword/index.tsx
@@ -1,8 +1,40 @@
 import * as C from "./styles.ts"
+import HomePageImg from "../../components/homepageImg";
+import logo from "../../assets//ps_orkut.png"
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
 
 const NewPassword = () => {
     return (
         <>
+            <Navbar thereIsLoggedOut={true} thereIsProfile={false} thereIsUserEdit={false}/>
+            <C.PageContainer>
+                <div>
+                    <HomePageImg/>
+                </div>
+
+                <C.NewPasswordMenu>
+                    <C.MenuHeader>
+                        <C.Logo src={logo}/>
+                        <C.h1>Nova senha</C.h1>
+                    </C.MenuHeader>
+
+                    <C.input type="text" id="code" name="code" placeholder="Informe o código"></C.input>
+                    <C.input type="password" id="password" name="password" placeholder="Nova Senha"></C.input>
+                    <C.input type="password" id="password-confirm" name="password-confirm" placeholder="Confirmar a senha"></C.input>
+
+                    <C.SaveButton>Salvar</C.SaveButton>
+
+                    {/*Vai ser necessario mudar isso no futuro para que se torne algo clicavel, pois no momento se trata somente de uma TAG <P></P>*/}
+                    <C.ClickText>
+                        <p>Lembrou sua Senha?</p>
+                    </C.ClickText>
+
+                    <C.AlternativeButton>Entrar com as credenciais</C.AlternativeButton>
+                </C.NewPasswordMenu>
+
+            </C.PageContainer>
+            <Footer/>
         </>
     )
 }
--- src/pages/NewPassword/styles.ts
+++ src/pages/NewPassword/styles.ts
@@ -1,1 +1,58 @@
 import styled from "styled-components";
+
+export const PageContainer = styled.div`
+
+`;
+
+export const NewPasswordMenu = styled.div`
+
+`;
+
+export const MenuHeader = styled.div`
+
+`;
+
+export const Logo = styled.img`
+
+`;
+
+export const h1 = styled.h1`
+
+`;
+
+export const input = styled.input`
+    
+`;
+
+export const SaveButton = styled.button`
+
+`;
+
+export const ClickText = styled.div `
+  color: #ED6D25;
+  font-family: Roboto Flex;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-top: 6.41rem;
+  display: flex;
+  align-self: center;
+`;
+
+export const AlternativeButton = styled.button`
+  width: 21rem;
+  height: 3.1875rem;
+  flex-shrink: 0;
+  border-radius: 0.5rem;
+  background: #EFF3F8;
+  border: none;
+  color: #ED6D25;
+  font-family: Roboto Flex;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  cursor: pointer;
+  margin-top: 0.94rem;
+`;
